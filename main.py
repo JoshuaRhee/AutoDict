fdee6ad06ebb3cd989c520195d3a047c6f2f66cd
JoshuaRhee <jh.rhee@kriss.re.kr>
2022-01-14 00:24:34 +0900

    Update to v1.1.1

--- main.py
+++ main.py
@@ -5,3 +5,10 @@ word = easygui.enterbox(msg='Enter a keyword to search:', title='AutoDict', defa
 
 if word != None:
     webbrowser.open(f'https://en.dict.naver.com/#/search?range=all&query={word}', new=1, autoraise=True)
+
+# HOW TO MAKE CODE TO EXE:
+# pyinstaller -w -F -n AutoDict --icon=.\icon.ico main.py
+# -w : supress console
+# -F : package to a single file
+# -n : name of the file
+# --icon= : set icon image
