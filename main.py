c679cfd45a31d6e3efcb472f1d88f84e95dbb1ca
JoshuaRhee <jh.rhee@kriss.re.kr>
2022-01-13 23:25:38 +0900

    Update to v1.0

---
+++ main.py
@@ -0,0 +1,6 @@
+import webbrowser
+import easygui
+
+word = easygui.enterbox(msg='Enter a keyword to search:', title='AutoDict', default='')
+
+webbrowser.open(f'https://en.dict.naver.com/#/search?range=all&query={word}', new=1, autoraise=True)
