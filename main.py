98f5effc3f5c38db75ffc5b4c031d36f76f1e984
JoshuaRhee <jh.rhee@kriss.re.kr>
2022-01-14 00:05:24 +0900

    Update to v1.1

--- main.py
+++ main.py
@@ -3,4 +3,5 @@ import easygui
 
 word = easygui.enterbox(msg='Enter a keyword to search:', title='AutoDict', default='')
 
-webbrowser.open(f'https://en.dict.naver.com/#/search?range=all&query={word}', new=1, autoraise=True)
+if word != None:
+    webbrowser.open(f'https://en.dict.naver.com/#/search?range=all&query={word}', new=1, autoraise=True)
